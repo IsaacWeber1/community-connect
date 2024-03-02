f82654293aab6bf4221d407164df21c110a98f36
IsaacWeber1 <isaacgweber@gmail.com>
2024-03-02 14:59:18 -0500

    rough template

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Form } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      <Form.Group controlId="reasonForTrip">
+
+      </Form.Group>
+      <Form.Group controlId="lengthOfTrip">
+        
+      </Form.Group>
     </div>
   );
 }
