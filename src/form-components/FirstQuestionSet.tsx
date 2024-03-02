b9775c5dacf1a0e2b1bd23ae1d4481b4412f9d04
Olivia <okarney@udel.edu>
2024-03-02 15:58:43 -0500

    tried to copy forms but saved this time!

--- src/form-components/FirstQuestionSet.tsx
+++ src/form-components/FirstQuestionSet.tsx
@@ -20,6 +20,70 @@ export function FirstQuestionSet(): JSX.Element {
                 onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPurpose(event.target.value)}>
                 </Form.Control>
             </Form.Group>
+
+            <Form.Group controlId="reasonForTrip">
+                <Form.Label>What is you reason for visiting?</Form.Label>
+                <Form.Control
+                value={purpose}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPurpose(event.target.value)}>
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="reasonForTrip">
+                <Form.Label>What is you reason for visiting?</Form.Label>
+                <Form.Control
+                value={purpose}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPurpose(event.target.value)}>
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="reasonForTrip">
+                <Form.Label>What is you reason for visiting?</Form.Label>
+                <Form.Control
+                value={purpose}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPurpose(event.target.value)}>
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="reasonForTrip">
+                <Form.Label>What is you reason for visiting?</Form.Label>
+                <Form.Control
+                value={purpose}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPurpose(event.target.value)}>
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="reasonForTrip">
+                <Form.Label>What is you reason for visiting?</Form.Label>
+                <Form.Control
+                value={purpose}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPurpose(event.target.value)}>
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="reasonForTrip">
+                <Form.Label>What is you reason for visiting?</Form.Label>
+                <Form.Control
+                value={purpose}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPurpose(event.target.value)}>
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="reasonForTrip">
+                <Form.Label>What is you reason for visiting?</Form.Label>
+                <Form.Control
+                value={purpose}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPurpose(event.target.value)}>
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="reasonForTrip">
+                <Form.Label>What is you reason for visiting?</Form.Label>
+                <Form.Control
+                value={purpose}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPurpose(event.target.value)}>
+                </Form.Control>
+            </Form.Group>
         </>
     )
 }
