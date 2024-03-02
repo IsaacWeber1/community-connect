b153014462dad5f1bbbeba1798af68194bb9d3aa
IsaacWeber1 <isaacgweber@gmail.com>
2024-03-02 15:55:43 -0500

    lots of forms

--- src/form-components/FirstQuestionSet.tsx
+++ src/form-components/FirstQuestionSet.tsx
@@ -4,12 +4,22 @@ import { Col, Form, Row } from "react-bootstrap";
 export function FirstQuestionSet(): JSX.Element {
     const [purpose, setPurpose] = useState<string>("");
     return (
-        <Form.Group controlId="reasonForTrip">
-            <Form.Label>What is you reason for visiting?</Form.Label>
-            <Form.Control
-            value={purpose}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPurpose(event.target.value)}>
-            </Form.Control>
-        </Form.Group>
+        <>
+            <Form.Group controlId="reasonForTrip">
+                <Form.Label>What is you reason for visiting?</Form.Label>
+                <Form.Control
+                value={purpose}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPurpose(event.target.value)}>
+                </Form.Control>
+            </Form.Group>
+            
+            <Form.Group controlId="reasonForTrip">
+                <Form.Label>What is you reason for visiting?</Form.Label>
+                <Form.Control
+                value={purpose}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPurpose(event.target.value)}>
+                </Form.Control>
+            </Form.Group>
+        </>
     )
 }
