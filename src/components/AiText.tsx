6c0956e97ac06df814e295c49a5feef5e1ec6c06
JacobW <jacobwhi@udel.edu>
2024-03-02 17:31:19 -0500

    aaaaa

---
+++ src/components/AiText.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const AiwithText = () => {
+    const genAI = new GoogleGenerativeAI('AIzaSyBLTpKC-aCVOe3CLEKXTb03qWp8C3jaXAQ');
+
+    const [search, setSearch] = useState('');
+    const [aiResponse, setResponse] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    /**
+     * Generative AI Call to fetch text insights
+     */
+    async function aiRun() {
+        setLoading(true);
+        setResponse('');
+        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+        const prompt = `tell me about your favorite anime`;
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        const text = response.text();
+        setResponse(text);
+        setLoading(false);
+    }
+
+    const handleChangeSearch = (e) => {
+        setSearch(e.target.value);
+    }
+
+    const handleClick = () => {
+        aiRun();
+    }
+
+    return (
+        <div>
+            <p>
+                HONKKKKK
+            </p>
+        </div>
+        // WILL ADD AFTER THE FUN STUFF
+    );
+};
+
+export default AiwithText;
