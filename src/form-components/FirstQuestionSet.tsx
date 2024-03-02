e21832a1bdcd5069e1a009d092a7dfb10dcf6417
IsaacWeber1 <isaacgweber@gmail.com>
2024-03-02 16:21:13 -0500

    creating states

--- src/form-components/FirstQuestionSet.tsx
+++ src/form-components/FirstQuestionSet.tsx
@@ -2,7 +2,13 @@ import React, { useState } from "react";
 import { Col, Form, Row } from "react-bootstrap";
 
 export function FirstQuestionSet(): JSX.Element {
+
     const [purpose, setPurpose] = useState<string>("");
+    const [duration, setDuration] = useState<number>(0);
+    const [needs, setNeeds] = useState<string[]>([]);
+    const [interests, setInterests] = useState<string[]>([]);
+    const [previouslyBeen, setPreviouslyBeen] = useState<boolean>(false);
+    const [bugdet, setBudget] = useState<number>(0);
     return (
         <>
             <Form.Group controlId="reasonForTrip">
