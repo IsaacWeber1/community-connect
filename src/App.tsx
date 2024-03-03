0a33c5e88a511e357e67da21b282386a022648a0
Zahra Temori <ZAHRATM@EMAIL>
2024-03-02 22:15:21 -0500

    added picture

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import { FirstQuestionSet } from './form-components/FirstQuestionSet';
 function App() {
   return (
     <div className="App">
+      <img src= "https://thumbs.dreamstime.com/b/word-welcome-many-languages-sign-word-welcome-many-languages-sign-224727308.jpg" alt= "Picture" 
+      width = {1600} 
+      height = {300}/>
       <h1>Connections AI</h1>
       <FirstQuestionSet></FirstQuestionSet>
       
