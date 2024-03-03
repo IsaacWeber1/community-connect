4b95770d0fa07f58f7288aaabd0190c6655e3efc
IsaacWeber1 <isaacgweber@gmail.com>
2024-03-03 03:08:39 -0500

    Logo2

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from 'C:\\Users\\isaac\\OneDrive\\Desktop\\Code\\Git\\community-connect\\src\\logo2.jpg';
 import './App.css';
 import { Form } from 'react-bootstrap';
 import { FirstQuestionSet } from './form-components/FirstQuestionSet';
-
+import { Header } from 'react-bootstrap/lib/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Connections AI</h1>
-      <FirstQuestionSet></FirstQuestionSet>
-      
-      
-    </div>
+    <>
+      <header>
+        <div className='App-header'>
+          <h1 className='App-heading1'>Explorer</h1>
+          <h3>^placeholder</h3>
+          <img src={logo} alt="App Logo" style={{
+            position: "relative"
+          }}/>
+        </div>
+      </header>
+      <div className="App">
+        <FirstQuestionSet></FirstQuestionSet>
+      </div>
+    </>
   );
 }
 export default App;
