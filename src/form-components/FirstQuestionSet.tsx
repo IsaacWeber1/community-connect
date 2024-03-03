58adefb96c9d84f9bcc2d49d7707459847055802
IsaacWeber1 <isaacgweber@gmail.com>
2024-03-02 19:54:58 -0500

    new questions

--- src/form-components/FirstQuestionSet.tsx
+++ src/form-components/FirstQuestionSet.tsx
@@ -3,9 +3,12 @@ import { Col, Form, Row } from "react-bootstrap";
 
 export function FirstQuestionSet(): JSX.Element {
 
+    const [destination, setDestination]= useState<string>("");
     const [purpose, setPurpose] = useState<string>("");
+    const [beliefs, setBeliefs] = useState<string>("");
+    const [languages, setLanguages]= useState<string>("");
     const [travellerCount, setTravellerCount] = useState<number>(1);
-    const [duration, setDuration] = useState<number>(0);
+    const [duration, setDuration] = useState<string>("");
     const [needs, setNeeds] = useState<string>("");
     const [locations, setLocations] = useState<string>("");
     const [previouslyBeen, setPreviouslyBeen] = useState<boolean>(false);
@@ -16,102 +19,145 @@ export function FirstQuestionSet(): JSX.Element {
     const [bugdet, setBudget] = useState<number>(0);
 
     return (
-        <>
+        <ol>
+            <h4>We will ask a feq questions to ensure your trip is personalized to your preferences</h4>
+            <br></br>
+            <Form.Group controlId="placeVisiting">
+                <li>
+                    <Form.Label>Where are you visiting?</Form.Label>
+                    <Form.Control
+                    value={destination}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setDestination(event.target.value)}>
+                    </Form.Control>
+                </li>
+            </Form.Group>
+            <br></br>
             <Form.Group controlId="reasonForTrip">
-                <Form.Label>What is you reason for visiting?</Form.Label>
-                <Form.Control
-                value={purpose}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPurpose(event.target.value)}>
-                </Form.Control>
+                <li>
+                    <Form.Label>What language(s) do you speak?</Form.Label>
+                    <Form.Control
+                    value={languages}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setLanguages(event.target.value)}>
+                    </Form.Control>
+                </li>
             </Form.Group>
-            <hr></hr>
-            <Form.Group controlId="numberTravellingWith">
-                <Form.Label>How many people are you travelling with?</Form.Label>
-                <Form.Control
-                type="number"
-                value={travellerCount}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setTravellerCount(parseInt(event.target.value))}>
-                </Form.Control>
-            </Form.Group>
-            <hr></hr>
-            <Form.Group controlId="howLongStaying">
-                <Form.Label>How long are you staying?</Form.Label>
-                <Form.Control
-                type="number"
-                value={duration}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setDuration(parseInt(event.target.value))}>
-                </Form.Control>
-            </Form.Group>
-            <hr></hr>
-            <Form.Group controlId="budgetForTrip">
-                <Form.Label>What is your budget?</Form.Label>
-                <Form.Control
-                type="number"
-                value={bugdet}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setBudget(parseInt(event.target.value))}>
-                </Form.Control>
-            </Form.Group>
-            <hr></hr>
+            <br></br>
             <Form.Group controlId="ageOfUser">
-                <Form.Label>How old are you?</Form.Label>
-                <Form.Control
-                type="number"
-                value={age}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setAge(parseInt(event.target.value))}>
-                </Form.Control>
+                <li>
+                    <Form.Label>Could you please share how old you are?</Form.Label>
+                    <Form.Control
+                    type="number"
+                    value={age}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setAge(parseInt(event.target.value))}>
+                    </Form.Control>
+                </li>
             </Form.Group>
-            <hr></hr>
-            <Form.Group controlId="needs">
-                <Form.Label>Do you have any specific needs? (Consider: food, phone, money, housing/lodging, job, language learning resources)</Form.Label>
-                <Form.Control
-                as="textarea"
-                rows={3}
-                value={needs}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setNeeds(event.target.value)}>
-                </Form.Control>
-            </Form.Group>
-            <hr></hr>
-            <Form.Group controlId="specificLocationPreferences">
-                <Form.Label>If you are travelling for fun, are there specific locations or sites you are interested in visiting?</Form.Label>
-                <Form.Control
-                as="textarea"
-                value={locations}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setLocations(event.target.value)}>
-                </Form.Control>
-            </Form.Group>
-            <hr></hr>
-            <Form.Check
-                type="checkbox"
-                id="previouslyTravelledToLocation"
-                label="Have you previously travelled to this location?"
-                checked={previouslyBeen}
-                onChange={() => setPreviouslyBeen(!previouslyBeen)}
-            />
-            <hr></hr>
-            <Form.Group controlId="interests/hobbies">
-                <Form.Label>What are your interests/hobbies?</Form.Label>
-                <Form.Control
-                type="textarea"
-                value={interests}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setInterests(event.target.value)}>
-                </Form.Control>
-            </Form.Group>
-            <hr></hr>
+            <br></br>
             <Form.Group controlId="homeCountry/Location">
-                <Form.Label>Where are you from?</Form.Label>
-                <Form.Control
-                value={origin}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setOrigin(event.target.value)}>
-                </Form.Control>
+                <li>
+                    <Form.Label>Where are you from?</Form.Label>
+                    <Form.Control
+                    value={origin}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setOrigin(event.target.value)}>
+                    </Form.Control>
+                </li>
             </Form.Group>
-            <hr></hr>
+            <br></br>
+            <Form.Group controlId="reasonForTrip">
+                <li>
+                    <Form.Label>What is the purpose of your travel?</Form.Label>
+                    <Form.Control
+                    value={purpose}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPurpose(event.target.value)}>
+                    </Form.Control>
+                </li>
+            </Form.Group>
+            <br></br>
+            <Form.Group controlId="stateReligiousBeliefs">
+                <li>
+                    <Form.Label>Is ther eanything you would like to share about your religious beliefs?</Form.Label>
+                    <Form.Control
+                    value={beliefs}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setBeliefs(event.target.value)}>
+                    </Form.Control>
+                </li>
+            </Form.Group>
+            <br></br>
+            <Form.Group controlId="numberTravellingWith">
+                <li>
+                    <Form.Label>How many people are you travelling with?</Form.Label>
+                    <Form.Control
+                    type="number"
+                    value={travellerCount}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setTravellerCount(parseInt(event.target.value))}>
+                    </Form.Control>
+                </li>
+            </Form.Group>
+            <br></br>
+            <Form.Group controlId="howLongStaying">
+                <li>
+                    <Form.Label>How long are you planning to stay?</Form.Label>
+                    <Form.Control
+                    value={duration}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setDuration(event.target.value)}>
+                    </Form.Control>
+                </li>
+            </Form.Group>
+            <br></br>
+            <Form.Group controlId="budgetForTrip">
+                <li>
+                    <Form.Label>What is your budget for this trip?</Form.Label>
+                    <Form.Control
+                    type="number"
+                    value={bugdet}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setBudget(parseInt(event.target.value))}>
+                    </Form.Control>
+                </li>
+            </Form.Group>
+            <br></br>
+            <Form.Group controlId="needs">
+                <li>
+                    <Form.Label>Do you have any specific needs? (Consider: food, phone, money, housing/lodging, job, language learning resources)</Form.Label>
+                    <Form.Control
+                    as="textarea"
+                    rows={3}
+                    value={needs}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setNeeds(event.target.value)}>
+                    </Form.Control>
+                </li>
+            </Form.Group>
+            <br></br>
+            <Form.Group controlId="specificLocationPreferences">
+                <li>
+                    <Form.Label>If you are travelling for fun, do you have any specific places or activities of interest?</Form.Label>
+                    <Form.Control
+                    as="textarea"
+                    value={locations}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setLocations(event.target.value)}>
+                    </Form.Control>
+                </li>
+            </Form.Group>
+            <br></br>
+            <Form.Group controlId="interests/hobbies">
+                <li>
+                    <Form.Label>What are your interests/hobbies?</Form.Label>
+                    <Form.Control
+                    type="textarea"
+                    value={interests}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setInterests(event.target.value)}>
+                    </Form.Control>
+                </li>
+            </Form.Group>            
+            <br></br>
             <Form.Group controlId="hasDietaryRestrictions">
-                <Form.Label>Do you have any dietary restrictions?</Form.Label>
-                <Form.Control
-                value={dietaryInfo}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setDietaryInfo(event.target.value)}>
-                </Form.Control>
+                <li>
+                    <Form.Label>Is ther eanything specific you hope to learn or discover while on your trip?</Form.Label>
+                    <Form.Control
+                    value={dietaryInfo}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setDietaryInfo(event.target.value)}>
+                    </Form.Control>
+                </li>
             </Form.Group>
-        </>
+        </ol>
     )
 }
