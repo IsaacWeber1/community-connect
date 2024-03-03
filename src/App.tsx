e020d6ed80bdb69ec960f394b7cc67131e1a8bdd
IsaacWeber1 <isaacgweber@gmail.com>
2024-03-03 10:53:54 -0500

    sssss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo2.jpg';
+import headerImg from './Background.jpg';
 import './App.css';
 import { Form } from 'react-bootstrap';
 import { FirstQuestionSet } from './form-components/FirstQuestionSet';
@@ -10,17 +10,9 @@ function App() {
     <>
       <header>
         <div className='App-header'>
-          <h1 className='App-heading1'>Explorer</h1>
-          <h3>^placeholder</h3>
-          <img src={logo} alt="App Logo" style={{
+          <img src={headerImg} alt="App Logo" style={{
             position: "relative"
           }}/>
-        
-        </div>
-        <div>
-        <img src= "https://thumbs.dreamstime.com/b/word-welcome-many-languages-sign-word-welcome-many-languages-sign-224727308.jpg" alt= "Picture" 
-        width = {1600} 
-        height = {300}/>
         </div>
       </header>
       <div className="App">
